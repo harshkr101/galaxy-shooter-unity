6e97f72fd189036894ca413a7d3ad30d8f99a8e8
harshkr101 <harsh.mca19.du@gmail.com>
2022-01-10 14:26:59 +0530

    adds enemy destruction animation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,21 @@ public class Enemy : MonoBehaviour
 {
     private float _speed = 6.0f;
     private Player _player;
+    private Animator _animator;
     
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        if (_player == null)
+        {
+            Debug.LogError("Player is null");
+        }
+        _animator = GetComponent<Animator>();
+        if (_animator == null)
+        {
+            Debug.LogError("Animator is null");
+        }
     }
 
     // Update is called once per frame
@@ -33,7 +43,11 @@ public class Enemy : MonoBehaviour
             {
                 player.Damage();
             }
-            Destroy(this.gameObject);
+            // trigger enemy destruction animation
+            _animator.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            // destroy enemy object
+            Destroy(this.gameObject,2.8f);
         }
         
         // destroy enemy and laser if laser collides with enemy
@@ -44,7 +58,11 @@ public class Enemy : MonoBehaviour
             {
                 _player.AddScore(10); // add player score on after destroying enemy
             }
-            Destroy(this.gameObject);
+            // trigger enemy destruction animation
+            _animator.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            // destroy enemy object
+            Destroy(this.gameObject,2.8f);
         }
     }
 }

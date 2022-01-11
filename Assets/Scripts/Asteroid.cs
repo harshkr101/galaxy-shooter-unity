0d016396e335790d835b941ba3014dff3c7338ff
harshkr101 <harsh.mca19.du@gmail.com>
2022-01-11 16:49:33 +0530

    adds asteroid explosion effect

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+   [SerializeField] private float _rotateSpeed = 20.0f;
+   [SerializeField] private GameObject _explosionPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotate the object
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag( "Laser"))
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject,0.25f);   
+        }
+    }
+}
